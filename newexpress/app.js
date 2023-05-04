948414eabdc49b59bbff44787311eb6fc282319b
Fardin2112 <fardinalam2112@gmail.com>
2023-05-04 23:51:08 +0530

    done some works of backend

--- newexpress/app.js
+++ newexpress/app.js
@@ -5,6 +5,7 @@ const port = 80;
 
 //EXPRESS SPECIFIC STUFF
 app.use('/static', express.static('static')) //For serving static file
+app.use(express.urlencoded())
 
 //PUG SPECIFIC STUFF
 app.set('view engine','pug')  //set the template engine as pug
@@ -15,6 +16,12 @@ app.get('/',(req,res)=>{
     const con = "This is the content of Web dev"
     const params = {'title':'Pubg  is the best game',"content":con}
     res.status(200).render('index',params)
+})
+app.post('/',(req,res)=>{
+    console.log(req.body)
+    const params = {'message':'Your form has been sumbitted succesfully'}
+    res.status(200).render('index',params)
+
 })
 
 //START THE SERVER
